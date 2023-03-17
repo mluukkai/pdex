8c42c0bf300c273659493745b66dc9c4f32f720b
Matti Luukkainen <mluukkai@iki.fi>
2023-03-17 13:42:31 +0200

    stuff

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 
-// Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
 app.get('/health', (req, res) => {
@@ -9,7 +8,7 @@ app.get('/health', (req, res) => {
 })
 
 app.get('/version', (req, res) => {
-  res.send('9') // change this string to ensure a new version deployed
+  res.send('10')
 })
 
 app.use(express.static('dist'))
